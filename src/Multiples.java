6f98ad2e034a83974c00e2d82634dbce8472faa0
Veda Kesarwani <vedakesarwani@gmail.com>
2023-09-11 22:51:10 -0400

    total multiples of 3 or 5 till 1000

---
+++ src/Multiples.java
@@ -0,0 +1,22 @@
+public class Multiples {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int total = 0;
+        while (i<1000) {
+            boolean multipleOf3 = i % 3 == 0;
+            boolean multipleOf5 = i % 5 == 0;
+
+            if (multipleOf3 || multipleOf5){
+                total += 1;
+            }
+            else{
+                total += 0;
+            }
+
+            i++;
+
+        }
+        System.out.println(total);
+    }
+}
